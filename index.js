c860853c49bdb6325f1a15e2fc669551ac6b4138
AsaadDadoush <asaaddaadouch246@gmail.com>
2022-11-19 20:04:25 +0300

    rolesselector

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ let bot = {
   owners: ["290544333843857418"]
 }
 
-// client.commands = new Discord.Collection()
-// client.events = new Discord.Collection()
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 client.slashcommands = new Discord.Collection()
-// client.buttons = new Discord.Collection()
+client.buttons = new Discord.Collection()
 
-// client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
-// client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
 client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
-// client.loadButtons = (bot, reload) => require("./handlers/buttons")(bot, reload)
+client.loadButtons = (bot, reload) => require("./handlers/buttons")(bot, reload)
 
-// client.loadEvents(bot, false)
-// client.loadCommands(bot, false)
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
 client.loadSlashCommands(bot, false)
-// client.loadButtons(bot, false)
+client.loadButtons(bot, false)
 
 
 module.exports = bot 
