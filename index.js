d3ba1dc0690ac9dfa66c76b52ae13d277de17d30
AsaadDadoush <asaaddaadouch246@gmail.com>
2022-11-19 17:50:15 +0300

    commands

--- index.js
+++ index.js
@@ -10,35 +10,59 @@ const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
 });
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+let bot = {
+  client, 
+  prefix: "h.",
+  owners: ["290544333843857418"]
+}
 
-client.on("messageCreate", (message) => {
-  if (message.content == "wasted") {
-    message.reply("wasted world");
-  }
-  if (message.content == "java version") {
-    message.reply(nodeVersion);
-  }
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+// client.slashcommands = new Discord.Collection()
+// client.buttons = new Discord.Collection()
 
-  if (message.content == "version") {
-    message.reply(bot_version.version);
-  }
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+// client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+// client.loadButtons = (bot, reload) => require("./handlers/buttons")(bot, reload)
 
-  if (message.content == "69") {
-    message.reply("NICE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-  }
-});
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+// client.loadSlashCommands(bot, false)
+// client.loadButtons(bot, false)
 
-const welcomeChannelId= "1043521776799461416"
 
-client.on("guildMemberAdd", async (member) => {
-  const img = await generateImage(member)
-  member.guild.channels.cache.get(welcomeChannelId).send({
-      content: `<@${member.id}> Welcome to the server!`,
-      files: [img]
-  })
-})
+module.exports = bot 
+
+// client.on("ready", () => {
+//   console.log(`Logged in as ${client.user.tag}`);
+// });
+
+// client.on("messageCreate", (message) => {
+//   if (message.content == "wasted") {
+//     message.reply("wasted world");
+//   }
+//   if (message.content == "java version") {
+//     message.reply(nodeVersion);
+//   }
+
+//   if (message.content == "version") {
+//     message.reply(bot_version.version);
+//   }
+
+//   if (message.content == "69") {
+//     message.reply("NICE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+//   }
+// });
+
+// const welcomeChannelId = "1043521776799461416";
+
+// client.on("guildMemberAdd", async (member) => {
+//   const img = await generateImage(member);
+//   member.guild.channels.cache.get(welcomeChannelId).send({
+//     content: `<@${member.id}> Welcome to the server!`,
+//     files: [img],
+//   });
+// });
 
 client.login(process.env.TOKEN);
